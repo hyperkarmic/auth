7ce47a998a11989f5404dffe74b8f744aad7cc4a
hyperkarmic <randy_pan@yahoo.com>
2021-06-02 19:21:14 +0100

    mongoose set up without env file

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+
 const app = express();
-// import routes
+const mongoose = require("mongoose");
+
+// connect to db
+mongoose.connect(
+  "mongodb+srv://hyperkarma:mibapab23@cluster0.1umtl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to db")
+);
 const authRoutes = require("./routes/auth");
 // route middlewares
 app.use("/api/user", authRoutes);
